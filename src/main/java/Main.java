be9829fd77dec047468465c6f8059993dd3f14bd
Kim Hye Lee <kimh3418@naver.com>
2024-01-21 12:43:46 +0900

    Implement main entry point
    실행될 메인 로직 작성하고 정렬 알고리즘 사용
    테스트는 실행 결과만 관측하고 있음, 검증 코드는 없음

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import logic.BubbleSort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        BubbleSort<String> sort = new BubbleSort<>();
+
+        System.out.println(sort.sort(Arrays.asList(args)));
+    }
+}
