b11583e7ded6bac47dbd33375bf74546a3c72611
Kim Hye Lee <kimh3418@naver.com>
2024-01-21 13:19:51 +0900

    Design #3 - Sort interface
    인터페이스 설계
    각 구현체가 인터페이스를 구현하는 구조로 수

--- src/main/java/logic/BubbleSort.java
+++ src/main/java/logic/BubbleSort.java
@@ -3,8 +3,9 @@ package logic;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BubbleSort<T extends Comparable<T>> {
+public class BubbleSort<T extends Comparable<T>> implements Sort<T>{
 
+    @Override
     public List<T> sort(List<T> list){
         // 입력, 출력 메모리 구분 -> 출력에 변화를 줘도 입력에 영향을 미치지 않음
         List<T> output= new ArrayList<>(list);
